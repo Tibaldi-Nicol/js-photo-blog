d93e4432a8f20367bf3ab21f80db1e2d6198d51b
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-07 16:35:13 +0200

    [UPD]Add-date-title-image-js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //aspetto che la pagina si carica tutta e dopo parte la funzione
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -6,13 +5,19 @@ document.addEventListener('DOMContentLoaded', function(){
     const apiUrl = 'https://lanciweb.github.io/demo/api/pictures/';
     //seleziono i div dove ci saranno le foto
     const photoDiv = document.querySelectorAll('.photo ');
+    //div per il titolo
     const titoloDiv = document.querySelectorAll('.pen')
+    //div per le date
+    const date =document.querySelectorAll('.date')
+
     //faccio la richiesta api con fetch
     fetch(apiUrl)
+
     //trasformo la risposta del server in json
     //questa funzione si esegue se la risposta arriva (.then)
     .then(response=> response.json())  //qui diventa in json(compatibile con jasvascript)
     //se arriva la risposta allora parte la funzione
+
     //quando arrivano i dati chiamali dati ricevuti
     .then(datiRicevuti=> {
 
@@ -31,8 +36,10 @@ document.addEventListener('DOMContentLoaded', function(){
             //index = posizione attuale (prima, seconda, terz e oltre).
 
             //data[index] = immagine da mettere in quel posto.
-            titoloDiv[index].textContent = datiRicevuti[index].title || 'Senza titolo';
+            date[index].textContent = datiRicevuti[index].date  ||'senza data'; //Aggiunta la data sopra il titolo
 
+            titoloDiv[index].textContent = datiRicevuti[index].title || 'Senza titolo';
+            
             //datiRicevuti[index].title => prende il titolo associato all'immagine.
 
             //titoloDiv[index] => seleziona il div corrispondente con classe .pen nella posizione giusta.
